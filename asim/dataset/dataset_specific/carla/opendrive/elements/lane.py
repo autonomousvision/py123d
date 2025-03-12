15c7e061c3e2f555ceb5b2549b47968c82ed6394
DanielDauner <daniel.dauner@gmail.com>
2025-03-12 11:24:57 +0100

    Push buggy and incomplete state

---
+++ asim/dataset/dataset_specific/carla/opendrive/elements/lane.py
@@ -0,0 +1,186 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Optional
+from xml.etree.ElementTree import Element
+
+import numpy as np
+import numpy.typing as npt
+
+
+@dataclass
+class Lanes:
+    lane_offsets: List[LaneOffset]
+    lane_sections: List[LaneSection]
+
+    def __post_init__(self):
+        self.lane_offsets.sort(key=lambda x: x.s, reverse=False)
+        self.lane_sections.sort(key=lambda x: x.s, reverse=False)
+
+    @classmethod
+    def parse(cls, lanes_element: Optional[Element]) -> Lanes:
+        args = {}
+        lane_offsets: List[LaneOffset] = []
+        for lane_offset_element in lanes_element.findall("laneOffset"):
+            lane_offsets.append(LaneOffset.parse(lane_offset_element))
+        args["lane_offsets"] = lane_offsets
+
+        lane_sections: List[LaneSection] = []
+        for lane_section_element in lanes_element.findall("laneSection"):
+            lane_sections.append(LaneSection.parse(lane_section_element))
+        args["lane_sections"] = lane_sections
+        return Lanes(**args)
+
+
+@dataclass
+class LaneOffset:
+    """Section 11.4"""
+
+    s: float
+    a: float
+    b: float
+    c: float
+    d: float
+
+    def __post_init__(self):
+        # NOTE: added assertion/filtering to check for element type or consistency
+        pass
+
+    @classmethod
+    def parse(cls, lane_offset_element: Element) -> LaneOffset:
+        args = {key: float(lane_offset_element.get(key)) for key in ["s", "a", "b", "c", "d"]}
+        return LaneOffset(**args)
+
+    @property
+    def polynomial_coefficients(self) -> npt.NDArray[np.float64]:
+        return np.array([self.a, self.b, self.c, self.d], dtype=np.float64)
+
+
+@dataclass
+class LaneSection:
+    s: float
+    left_lanes: List[Lane]
+    center_lanes: List[Lane]
+    right_lanes: List[Lane]
+
+    def __post_init__(self):
+        self.left_lanes.sort(key=lambda x: x.id, reverse=False)
+        self.right_lanes.sort(key=lambda x: x.id, reverse=True)
+        # NOTE: added assertion/filtering to check for element type or consistency
+
+    @classmethod
+    def parse(cls, lane_section_element: Optional[Element]) -> LaneSection:
+        args = {}
+        args["s"] = float(lane_section_element.get("s"))
+
+        left_lanes: List[Lane] = []
+        if lane_section_element.find("left") is not None:
+            for lane_element in lane_section_element.find("left").findall("lane"):
+                left_lanes.append(Lane.parse(lane_element))
+        args["left_lanes"] = left_lanes
+
+        center_lanes: List[Lane] = []
+        if lane_section_element.find("center") is not None:
+            for lane_element in lane_section_element.find("center").findall("lane"):
+                center_lanes.append(Lane.parse(lane_element))
+        args["center_lanes"] = center_lanes
+
+        right_lanes: List[Lane] = []
+        if lane_section_element.find("right") is not None:
+            for lane_element in lane_section_element.find("right").findall("lane"):
+                right_lanes.append(Lane.parse(lane_element))
+        args["right_lanes"] = right_lanes
+
+        return LaneSection(**args)
+
+
+@dataclass
+class Lane:
+
+    id: int
+    type: str
+    level: bool
+
+    widths: List[Width]
+    road_marks: List[RoadMark]
+
+    predecessor: Optional[int] = None
+    successor: Optional[int] = None
+
+    def __post_init__(self):
+        self.widths.sort(key=lambda x: x.s_offset, reverse=False)
+        # NOTE: added assertion/filtering to check for element type or consistency
+
+    @classmethod
+    def parse(cls, lane_element: Optional[Element]) -> Lane:
+        args = {}
+        args["id"] = int(lane_element.get("id"))
+        args["type"] = lane_element.get("type")
+        args["level"] = lane_element.get("level")
+
+        if lane_element.find("link") is not None:
+            if lane_element.find("link").find("predecessor") is not None:
+                args["predecessor"] = int(lane_element.find("link").find("predecessor").get("id"))
+            if lane_element.find("link").find("successor") is not None:
+                args["successor"] = int(lane_element.find("link").find("successor").get("id"))
+
+        widths: List[Width] = []
+        for width_element in lane_element.findall("width"):
+            widths.append(Width.parse(width_element))
+        args["widths"] = widths
+
+        road_marks: List[Width] = []
+        for road_mark_element in lane_element.findall("roadMark"):
+            road_marks.append(RoadMark.parse(road_mark_element))
+        args["road_marks"] = road_marks
+
+        return Lane(**args)
+
+
+@dataclass
+class Width:
+    s_offset: Optional[float] = None
+    a: Optional[float] = None
+    b: Optional[float] = None
+    c: Optional[float] = None
+    d: Optional[float] = None
+
+    @classmethod
+    def parse(cls, width_element: Optional[Element]) -> Width:
+        args = {}
+        args["s_offset"] = float(width_element.get("sOffset"))
+        args["a"] = float(width_element.get("a"))
+        args["b"] = float(width_element.get("b"))
+        args["c"] = float(width_element.get("c"))
+        args["d"] = float(width_element.get("d"))
+        return Width(**args)
+
+    @property
+    def polynomial_coefficients(self) -> npt.NDArray[np.float64]:
+        return np.array([self.a, self.b, self.c, self.d], dtype=np.float64)
+
+
+@dataclass
+class RoadMark:
+    s_offset: float = None
+    type: str = None
+    material: Optional[str] = None
+    color: Optional[str] = None
+    width: Optional[float] = None
+    lane_change: Optional[str] = None
+
+    def __post_init__(self):
+        # NOTE: added assertion/filtering to check for element type or consistency
+        pass
+
+    @classmethod
+    def parse(cls, road_mark_element: Optional[Element]) -> RoadMark:
+        args = {}
+        args["s_offset"] = float(road_mark_element.get("sOffset"))
+        args["type"] = road_mark_element.get("type")
+        args["material"] = road_mark_element.get("material")
+        args["color"] = road_mark_element.get("color")
+        if road_mark_element.get("width") is not None:
+            args["width"] = float(road_mark_element.get("width"))
+        args["lane_change"] = road_mark_element.get("lane_change")
+        return RoadMark(**args)
---
+++ asim/dataset/dataset_specific/carla/opendrive/elements/reference.py
@@ -0,0 +1,181 @@
+from __future__ import annotations
+
+import warnings
+from dataclasses import dataclass
+from functools import cached_property
+from typing import List, Optional, Union
+from xml.etree.ElementTree import Element
+
+import numpy as np
+import numpy.typing as npt
+
+from asim.common.geometry.array_representations import SE2Index
+from asim.common.geometry.utils import normalize_angle
+from asim.dataset.dataset_specific.carla.opendrive.elements.geometry import Arc, Geometry, Line
+from asim.dataset.dataset_specific.carla.opendrive.elements.lane import LaneOffset
+
+
+@dataclass
+class PlanView:
+
+    geometries: List[Geometry]
+
+    def __post_init__(self):
+        # NOTE: added assertion/filtering to check for element type or consistency
+        self.geometries.sort(key=lambda x: x.s, reverse=False)
+
+    @classmethod
+    def parse(cls, plan_view_element: Optional[Element]) -> PlanView:
+
+        args = {}
+        geometries: List[Geometry] = []
+        for geometry_element in plan_view_element.findall("geometry"):
+            if geometry_element.find("line") is not None:
+                geometry = Line.parse(geometry_element)
+            elif geometry_element.find("arc") is not None:
+                geometry = Arc.parse(geometry_element)
+            else:
+                warnings.warn(f"Warning..... Unknown geometry type {str(geometry_element)}")
+                continue
+            geometries.append(geometry)
+        args["geometries"] = geometries
+        return PlanView(**args)
+
+    @cached_property
+    def geometry_lengths(self) -> npt.NDArray[np.float64]:
+        return np.cumsum([0.0] + [geo.length for geo in self.geometries], dtype=np.float64)
+
+    @property
+    def length(self) -> float:
+        return float(self.geometry_lengths[-1])
+
+    def interpolate_se2(self, s: float, t: float = 0.0) -> npt.NDArray[np.float64]:
+
+        try:
+            # get index of geometry which is at s_pos
+            mask = self.geometry_lengths > s
+            sub_idx = np.argmin(self.geometry_lengths[mask] - s)
+            geo_idx = np.arange(self.geometry_lengths.shape[0])[mask][sub_idx] - 1
+        except ValueError:
+
+            # # warnings.warn(f"{s} clipped to {0.0} {self.geometry_lengths[-1]}")
+            # print(f"{s} clipped to {0.0} {self.geometry_lengths[-1]}")
+            # s = np.clip(s, 0.0, self.geometry_lengths[-1], dtype=np.float64)
+            # # geo_idx = self.geometry_lengths.size - 2
+            # geo_idx = 0
+            # # geo_idx = max(0, self.geometry_lengths.size - 2)
+
+            # s_pos is after last geometry because of rounding error
+            if np.isclose(s, self.geometry_lengths[-1], 0.01, 0.01):  # todo parameter
+                geo_idx = self.geometry_lengths.size - 2
+            else:
+                raise Exception(
+                    f"Tried to calculate a position outside of the borders of the reference path at s={s}"
+                    f", but path has only length of l={self.length}"
+                )
+                # print(f"{s} clipped to {0.0} {self.geometry_lengths[-1]}")
+                # s = np.clip(s, 0.0, self.geometry_lengths[-1], dtype=np.float64)
+                # geo_idx = self.geometry_lengths.size - 2
+                # # geo_idx = 0
+
+        return self.geometries[geo_idx].interpolate_se2(s - self.geometry_lengths[geo_idx], t)
+
+
+@dataclass
+class Border:
+
+    reference: Union[Border, PlanView]
+    width_coefficient_offsets: List[float]
+    width_coefficients: List[List[float]]
+
+    s_offset: float = 0.0
+
+    # NOTE: loaded in __post_init__
+    length: Optional[float] = None
+
+    def __post_init__(self):
+        # NOTE: added assertion/filtering to check for element type or consistency
+        self.length = float(self.reference.length)
+
+    @classmethod
+    def from_plan_view(cls, plan_view: PlanView, lane_offsets: List[LaneOffset]) -> Border:
+        args = {}
+        args["reference"] = plan_view
+
+        width_coefficient_offsets = []
+        width_coefficients = []
+
+        # Lane offsets will be coeffs
+        # this has to be done if the reference path has the laneoffset attribute
+        # and thus is different to the geometry described in the plan_view
+        # openDRIVE lets multiple laneOffsets start at the same position
+        # but only the last one counts -> delete all previous ones
+        if any(lane_offsets):
+            for lane_offset in lane_offsets:
+                if lane_offset.s in width_coefficient_offsets:
+                    # offset is already there, delete previous entries
+                    idx = width_coefficient_offsets.index(lane_offset.s)
+                    del width_coefficient_offsets[idx]
+                    del width_coefficients[idx]
+                width_coefficient_offsets.append(lane_offset.s)
+                width_coefficients.append(lane_offset.polynomial_coefficients)
+        else:
+            width_coefficient_offsets.append(0.0)
+            width_coefficients.append([0.0])
+
+        args["width_coefficient_offsets"] = width_coefficient_offsets
+        args["width_coefficients"] = width_coefficients
+
+        return Border(**args)
+
+    def _get_width_index(self, s: float, is_last_pos: bool) -> float:
+        """Get the index of the width which applies at position s_pos.
+
+        :param s_pos: Position on border in curve_parameter ds
+        :param is_last_pos: Whether s_pos is the last position
+        :return: Index of width that applies at position s_pos
+        """
+
+        return next(
+            (
+                self.width_coefficient_offsets.index(n)
+                for n in self.width_coefficient_offsets[::-1]
+                if ((n <= s and (not is_last_pos or s == 0)) or (n < s and is_last_pos))
+            ),
+            len(self.width_coefficient_offsets) - 1,
+        )
+
+    def interpolate_se2(self, s: float, t: float = 0.0) -> npt.NDArray[np.float64]:
+        # Last reference has to be a reference geometry (PlanView)
+        # Offset of all inner lanes (Border)
+        # calculate position of reference border
+        if np.isclose(s, 0):
+            s = 0
+
+        try:
+            se2 = self.reference.interpolate_se2(self.s_offset + s)
+        except TypeError:
+            se2 = self.reference.interpolate_se2(np.round(self.s_offset + s, 3))
+
+        if len(self.width_coefficients) == 0 or len(self.width_coefficient_offsets) == 0:
+            raise Exception("No entries for width definitions.")
+
+        # Find correct coefficients
+        # find which width segment is at s_pos
+
+        width_idx = self._get_width_index(s, is_last_pos=False)
+        # width_idx = min(width_idx, len(self.width_coefficient_offsets)-1)
+        # Calculate width at s_pos
+        distance = (
+            np.polynomial.polynomial.polyval(
+                s - self.width_coefficient_offsets[width_idx],
+                self.width_coefficients[width_idx],
+            )
+            + t
+        )
+        ortho = normalize_angle(se2[SE2Index.HEADING] + np.pi / 2)
+        se2[SE2Index.X] += distance * np.cos(ortho)
+        se2[SE2Index.Y] += distance * np.sin(ortho)
+
+        se2[SE2Index.HEADING] = normalize_angle(se2[SE2Index.HEADING])
+        return se2
