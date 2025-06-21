1543266f1539f07ceb4e9e11f708e182011395ce
Daniel Dauner <d.dauner@gmx.de>
2025-06-21 14:01:47 +0200

    Push SMART baseline for sim agents. Current code not fully functional but imports and environment is fixed. (#25)

---
+++ asim/training/models/sim_agent/smart/layers/__init__.py
@@ -0,0 +1,3 @@
+# from smart.layers.attention_layer import AttentionLayer
+# from smart.layers.fourier_embedding import FourierEmbedding, MLPEmbedding
+# from smart.layers.mlp_layer import MLPLayer
---
+++ asim/training/models/sim_agent/smart/utils/config.py
@@ -0,0 +1,19 @@
+import os
+
+import easydict
+import yaml
+
+
+def load_config_act(path):
+    """load config file"""
+    with open(path, "r") as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    return easydict.EasyDict(cfg)
+
+
+def load_config_init(path):
+    """load config file"""
+    path = os.path.join("init/configs", f"{path}.yaml")
+    with open(path, "r") as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    return cfg
